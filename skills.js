8ae320d71032bba54e363d103ce2acb92e43292a
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-28 19:50:04 -0400

    Update skills.js

--- skills.js
+++ skills.js
@@ -23,13 +23,7 @@ int countNodes(Node head){
 
 public static class Queue   {
 
-private static class Node {
-    private int data;
-    private Node next;
-    private Node (int data {
-        this.data = data;
-    }
-}
+
 
 private Node head; // remove from head
 private Node tail; // add things here 

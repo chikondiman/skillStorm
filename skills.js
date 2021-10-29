565a610df1890183e9e0e3821cb6388326d64f29
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-29 12:13:21 -0400

    Update skills.js

--- skills.js
+++ skills.js
@@ -78,7 +78,3 @@ only certain parts are shwon to user, hiding implementation details
 //  capacity, we can add more elements.
 
 
-// a linked list is a linear collection of data elements 
-// whose order is not given by their physical placement in memory.
-//  Instead, each element points to the next. It is a data structure 
-//  consisting of a collection of nodes which together represent a sequence.
